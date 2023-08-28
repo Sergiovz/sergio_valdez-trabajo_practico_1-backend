889e574ce7bdbfb142dbe168e50ca02cbc072eb4
sergio valdez <sergiovaldez599@gmail.com>
2023-08-28 04:00:51 -0300

    fix(develop): Autenticación de la base de datos y correciones en el servidor

--- app.js
+++ app.js
@@ -8,10 +8,19 @@ const port = process.env.PORT || 3000
 //Implementamos Middlewares
 app.use(express.json());
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + 'Public'));
 
 //Routes
-app.use();
+app.use(require('./routes/routersProducts'));
 
 //Iniciar el servidor
-app.listen(port, () => console.log(`Servidor corriendo en el puerto http://localhost:${port}`));
+app.listen(port,async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Conección exitosa a la Base de Datos');
+    } catch (error) {
+        console.error('Problemas para conectar a la Base de Datos:', error);
+    } finally {
+        console.log(`Servidor corriendo en el puerto http://localhost:${port}`);
+    }
+});
